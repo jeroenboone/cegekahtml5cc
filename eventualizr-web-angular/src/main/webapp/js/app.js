b845ccfbc7f5b492f30a4f8f7ccefd2f4b7f5ec0
Jeroen <jeroen.boone.cegeka@outlook.com>
2013-05-15 13:21:10 +0200

    jeroenb : angular-http-auth setup

--- eventualizr-web-angular/src/main/webapp/js/app.js
+++ eventualizr-web-angular/src/main/webapp/js/app.js
@@ -2,10 +2,14 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('eventualizrApp', ['ngResource', 'eventualizrApp.filters', 'eventualizrApp.services', 'eventualizrApp.directives']).
+angular.module('eventualizrApp', ['ngResource', 'eventualizrApp.filters', 'eventualizrApp.services', 'eventualizrApp.directives',
+                                  'http-auth-interceptor',
+                                  'content-mocks'
+                                  ]).
   config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/meetings', {templateUrl: 'partials/meeting-list.html', controller: MeetingListController});
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/session', {templateUrl: 'partials/session.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/meetings'});
   }]);
--- eventualizr-web-angular/src/main/webapp/js/controllers.js
+++ eventualizr-web-angular/src/main/webapp/js/controllers.js
@@ -17,3 +17,38 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+
+function SessionCtrl($scope, $http, authService) {
+    window.scope =  $scope;
+
+    $scope.publicContent = [];
+    $scope.restrictedContent = [];
+
+    $scope.publicAction = function() {
+      $http.post('data/public', $scope.publicData).success(function(response) {
+        $scope.publicContent.push(response);
+      });
+    }
+
+    $scope.restrictedAction = function() {
+      $http.post('data/protected', $scope.restrictedData).success(function(response) {
+        // this piece of code will not be executed until user is authenticated
+        $scope.restrictedContent.push(response);
+      });
+    }
+
+    $scope.logout = function() {
+      $http.post('auth/logout').success(function() {
+        $scope.restrictedContent = [];
+      });
+    }
+
+    $scope.login = function() {
+      $http.post('auth/login').success(function() {
+        authService.loginConfirmed();
+      });
+    }
+
+}
+SessionCtrl.$inject = ['$scope', '$http', 'authService'];
